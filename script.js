29d2e65c643f2870aaa6d4de413164d5bf3d0621
yande736 <yandee736@gmail.com>
2024-11-28 16:06:46 +0800

    update 2

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ window.addEventListener("load", function () {
 });
 
 const urlParams = new URLSearchParams(window.location.search);
-const nama = urlParams.get("n") || "";
+const nama = urlParams.get("to") || "";
 const pronoun = urlParams.get("p") || "Bapak/Ibu/Saudara/i";
 const namaContainer = document.querySelector(".hero h4 span");
 namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, ",");
